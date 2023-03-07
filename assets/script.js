7eb2fc26be04699a692df5eb67ec1a97bf8afcd2
hollywood2fresh <mael.boilevepro@gmail.com>
2023-03-07 17:07:10 +0100

    ajout fonction slideSuivant

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,37 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ];
+
+// permet de gérer les images du slider 
+const bannerImg = document.querySelector('.banner-img');
+const attribut = bannerImg.getAttribute('src');
+// bannerImg.setAttribute('src', `./assets/images/slideshow/${slides[0].image}`);
+
+
+// permet de gerer le texte du slider 
+const bannerText = document.querySelector('#banner p');
+// bannerText.innerHTML = slides[0].tagLine
+
+
+const suivant = document.querySelector('.arrow_right');
+const precedent = document.querySelector('.arrow_left');
+
+
+let index = 0;
+
+
+function slideSuivant() {
+	bannerImg.setAttribute('src', `./assets/images/slideshow/${slides[index].image}`);
+	bannerText.innerHTML = slides[index].tagLine;
+	if(index < slides.length - 1) {
+		index++;
+	} else {
+		index = 0;
+	}
+	console.log(index);
+};
+
+suivant.addEventListener('click', slideSuivant)
+
+
+// slides < slides.length - 1
